8253216f4ec25d52138e21f988a630fae0f8f214
Tim Jarratt <tjarratt@gmail.com>
2013-07-12 23:01:39 -0700

    Animate in spheres
    Could use some easing, possibly via TWEEN.js ?

--- webgl-bubblechart/app/assets/javascripts/webgl-bubblechart.js
+++ webgl-bubblechart/app/assets/javascripts/webgl-bubblechart.js
@@ -4,6 +4,8 @@ window.onload = function() {
     var scene = new THREE.Scene();
     var renderer = new THREE.WebGLRenderer();
 
+    var objects = [];
+
     var bChart = function(containerId, data) {
       this.data = data;
       this.container = document.getElementById(containerId);
@@ -27,13 +29,45 @@ window.onload = function() {
         var bubble = new THREE.Mesh(new THREE.SphereGeometry(radius, numSegments, numSegments), new THREE.MeshNormalMaterial());
         bubble.overdraw = true;
         bubble.position.set(d.x * scale, d.y * scale, d.z);
-        console.log(d.x * scale, d.y * scale, d.z);
+        bubble.finalRadius = radius;
         scene.add(bubble);
+        objects.push(bubble);
       });
     };
 
-    bChart.prototype.render = function() {
+    var lastTime;
+    var timeToAnimate = 1000;
+    var scaleFactor = Math.PI / 2.0 / timeToAnimate;
+    function animate() {
+      lastTime = lastTime || (new Date().getTime());
+      var time = new Date().getTime();
+      var timeDiff = time - lastTime;
+
+      if (timeDiff > timeToAnimate) {
+        objects.forEach(function(o) {
+          o.scale.set(1, 1, 1);
+        });
+
+        return renderer.render(scene, camera);
+      }
+
+      var factor = Math.abs(Math.sin(timeDiff * scaleFactor));
+      objects.forEach(function(o) {
+        o.scale.set(factor, factor, factor);
+      });
+
       renderer.render(scene, camera);
+      requestAnimationFrame(function() { animate(); });
+    };
+
+    bChart.prototype.render = function(options) {
+      options = options || {};
+      if (options.animate) {
+        requestAnimationFrame(function() { animate(); } );
+      }
+      else {
+        renderer.render(scene, camera);
+      }
     };
 
     return bChart;
@@ -41,6 +75,8 @@ window.onload = function() {
 
   var data = [];
   for (var i = 0; i < 55; ++i) {
+    // todo: it shouldn't matter what the units are here
+    // it doesn't make sense to hard code values in between
     data.push({
       size: 5 + Math.random() * 15,
       x: Math.random() * 100,
@@ -49,5 +85,5 @@ window.onload = function() {
     });
   }
   var chart = new BubbleChart('container', data);
-  chart.render();
+  chart.render({animate: true});
 };
